72f626a4158d7d1aabd86c951bd4c41e94ab7f6b
huangqilong <453220567@qq.com>
2015-07-09 14:33:54 +0800

    定时器修改

--- Dasher/src/com/dasher/servlet/AutoJobTask.java
+++ Dasher/src/com/dasher/servlet/AutoJobTask.java
@@ -1,10 +1,18 @@
 package com.dasher.servlet;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.dasher.service.MenuService;
+
+
 public class AutoJobTask {
+	@Autowired
+	private MenuService menuService;
 	
     public void doBiz() {
 	  // 执行业务逻辑
 	  // ........
-    	//System.out.print("asasas");
+    	System.out.print("asasas");
+    	menuService.getListOverTime();
     }
 }
